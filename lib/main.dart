a563184060c8c5de10eec2e7202d702224c8d9c7
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-08-12 08:53:08 +0530

    number and email added

--- lib/main.dart
+++ lib/main.dart
@@ -11,37 +11,104 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Color.fromARGB(255, 14, 14, 14),
-          body:
-          
-           SafeArea(
-            child: Column(
-              children: [
-                CircleAvatar(
-                  radius: 70.0,
-                  backgroundImage: AssetImage('images/arc.jpeg'),
+        backgroundColor: Color.fromARGB(255, 14, 14, 14),
+        body: SafeArea(
+          child: 
+          Column(
+            children: [
+              CircleAvatar(
+                radius: 70.0,
+                backgroundImage: AssetImage('images/arc.jpeg'),
+              ),
+              Text(
+                'Aravind',
+                style: TextStyle(
+                  fontFamily: 'VictorMono',
+                  fontSize: 40.0,
+                  color: Color.fromARGB(255, 126, 190, 239),
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  'Aravind',
-                  style: TextStyle(
-                    fontFamily: 'VictorMono',
-                    fontSize: 40.0,
-                    color: Color.fromARGB(255, 43, 106, 155),
-                    fontWeight: FontWeight.bold,
+              ),
+              Text(
+                'APP DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Handjet',
+                  fontSize: 20.0,
+                  letterSpacing: 0,
+                  color: Color.fromARGB(255, 254, 254, 254),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              //Container for first widget email
+              Container(
+                color: Color.fromARGB(255, 58, 74, 82),
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: 
+                Row(
+                  children: [
+                  SizedBox(
+                    width: 20.0,
                   ),
-                ),
-                Text(
-                  'APP DEVELOPER',
-                  style: TextStyle(
-                    fontFamily: 'Handjet',
-                    fontSize: 20.0,
-                    color: Color.fromARGB(255, 238, 57, 57),
-                    fontWeight: FontWeight.bold,
+                  Icon(
+                    Icons.phone,
+                    color: Color.fromARGB(255, 126, 190, 239),
                   ),
+                  Text(
+                    '+91 6382764801',
+                    style: TextStyle(
+                      fontFamily: 'VictorMono',
+                      fontSize: 15.0,
+                      color: Color.fromARGB(255, 126, 190, 239),
+                      //fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                
+
+                  //Container for second widget email
+
+                  
+                ]),
+              ),
+              //Second container for email
+              Container(
+                color: Color.fromARGB(255, 58, 74, 82),
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child:
+                 Row(
+                  children: [
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Icon(
+                    Icons.email,
+                    color: Color.fromARGB(255, 126, 190, 239),
+                  ),
+                  SizedBox(
+                    width: 20.0,),
+                  Text(
+                    'aravindraj5054@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'Titllium Web',
+                      fontSize: 20.0,
+                      color: Color.fromARGB(255, 126, 190, 239),
+                      fontStyle: FontStyle.italic
+                    ),
+                  ),
+                
+
+                  //Container for second widget email
+
+                  
+                ],
                 ),
-              ],
-            ),
-          )),
+                
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
